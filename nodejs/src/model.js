553f12da1810b431c4e994d22015f78d86b848ec
Orion Buske <orion.buske@gmail.com>
2021-10-12 03:52:28 -0400

    Update to match latest conceptual model

--- nodejs/src/model.js
+++ nodejs/src/model.js
@@ -11,9 +11,12 @@ var Identifier = proto.lookupType('org.ga4gh.pedigree.v1.Identifier');
 
 const Ontology = require('./ontology.js'); 
 
-MOTHER = Concept.create(Ontology.IS_BIOLOGICAL_MOTHER);
-FATHER = Concept.create(Ontology.IS_BIOLOGICAL_FATHER);
-
+IS_MOTHER = Concept.create(Ontology.IS_BIOLOGICAL_MOTHER);
+IS_FATHER = Concept.create(Ontology.IS_BIOLOGICAL_FATHER);
+GENDER_FEMALE = Concept.create({id: 'female'});
+GENDER_MALE = Concept.create({id: 'male'});
+GENDER_OTHER = Concept.create({id: 'other'});
+GENDER_UNKNOWN = Concept.create({id: 'unknown'});
 
 var Model = function(data) {
   this.pedigree = Pedigree.create(data);
@@ -40,12 +43,12 @@ Model.prototype = {
   // These two helper methods and the use of F/M concept ids is a placeholder
   isFemale: function(id) {
     var individual = this.getIndividual(id);
-    return (individual && individual.hasOwnProperty('gender') && individual.gender.id === "F");
+    return (individual && individual.hasOwnProperty('gender') && individual.gender.id === GENDER_FEMALE.id);
   },
 
   isMale: function(id) {
     var individual = this.getIndividual(id);
-    return (individual && individual.hasOwnProperty('gender') && individual.gender.id === "M");
+    return (individual && individual.hasOwnProperty('gender') && individual.gender.id === GENDER_MALE.id);
   },
 
   updateIndividual: function(id, properties) {
@@ -56,15 +59,25 @@ Model.prototype = {
     - affected
     - externalID
     - lifeStatus (not yet supported)
-    - disorders (not yet supported)
-    - evaluated (not yet supported)
     */
     var individual = this.getIndividual(id);
     if (properties.hasOwnProperty('name')) {
       individual.name = properties.name;
     }
     if (properties.hasOwnProperty('gender')) {
-      individual.gender = Concept.create({'id': properties.gender})
+      switch (properties.gender) {
+        case 'F':
+          individual.gender = GENDER_FEMALE;
+          break;
+        case 'M':
+          individual.gender = GENDER_MALE;
+          break;
+        case 'O':
+          individual.gender = GENDER_OTHER;
+          break;
+        default:
+          individual.gender = GENDER_UNKNOWN;
+      }
     }
     if (properties.hasOwnProperty('externalID')) {
       individual.identifiers = []
@@ -82,24 +95,24 @@ Model.prototype = {
   // motherId or fatherId may be null
   addChild: function(motherId, fatherId, childId) {
     if (motherId !== null) {
-      this.pedigree.relationships.push(Relationship.create({individual: childId, relation: MOTHER, relative: motherId}));
+      this.pedigree.relationships.push(Relationship.create({subject: motherId, relationship: IS_MOTHER, relative: childId}));
     }
     if (fatherId !== null) {
-      this.pedigree.relationships.push(Relationship.create({individual: childId, relation: FATHER, relative: fatherId}));
+      this.pedigree.relationships.push(Relationship.create({subject: fatherId, relationship: IS_FATHER, relative: childId}));
     }
   },
 
   // [father, mother]
   getParents: function(id) {
-    var fatherships = this.pedigree.relationships.filter(rel => rel.individual == id && rel.relation.id == FATHER.id);
-    var motherships = this.pedigree.relationships.filter(rel => rel.individual == id && rel.relation.id == MOTHER.id);
+    var fatherships = this.pedigree.relationships.filter(rel => rel.relationship.id == IS_FATHER.id && rel.relative == id);
+    var motherships = this.pedigree.relationships.filter(rel => rel.relationship.id == IS_MOTHER.id && rel.relative == id);
     var father = null;
     var mother = null;
     if (fatherships.length === 1) {
-      father = fatherships[0].relative;
+      father = fatherships[0].subject;
     }
     if (motherships.length === 1) {
-      mother = motherships[0].relative;
+      mother = motherships[0].subject;
     }
     if (father !== null || mother !== null) {
       return [father, mother];
